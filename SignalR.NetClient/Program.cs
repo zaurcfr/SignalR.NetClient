53be3e8dd4037719610912979e79ac43c68d2228
Zaur Jafarov <Cefe_a8xp@itstep.edu.az>
2022-02-24 21:42:36 +0400

    Add project files.

---
+++ SignalR.NetClient/Program.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.SignalR.Client;
+using System;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace SignalR.NetClient
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var connection = new HubConnectionBuilder().WithAutomaticReconnect().WithUrl("https://localhost:5001/chatHub").Build();
+            await connection.StartAsync();
+
+            connection.On<string>("Open", (message) =>
+            {
+                Console.WriteLine(message);
+            });
+            Console.ReadLine();
+        }
+    }
+}
